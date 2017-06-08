67583c189d22db3e7a0cec32eb276c0484ba7b8b
Ducpm <minhducpham.it@gmail.com>
2017-06-08 17:11:18 +0700

    add

---
+++ js/common.js
@@ -0,0 +1,14 @@
+(function($){
+  "use strict";
+
+  $('.selectpicker').selectpicker({
+    
+  });
+
+  $(".sidebar-toggle").click(function() {
+    /* Act on the event */
+    $("body").toggleClass("sidebar-collapse");
+  });
+
+
+})(jQuery); // End of use strict
