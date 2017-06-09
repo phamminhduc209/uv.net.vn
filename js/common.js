3cce64cb80bac9474867d570a206d718ebe0e58f
Ducpm <minhducpham.it@gmail.com>
2017-06-09 17:01:22 +0700

    add

--- js/common.js
+++ js/common.js
@@ -1,14 +1,29 @@
 (function($){
-  "use strict";
+	"use strict";
 
-  $('.selectpicker').selectpicker({
-    
-  });
+	// Select picker
+	$('.selectpicker').selectpicker({});
 
-  $(".sidebar-toggle").click(function() {
-    /* Act on the event */
-    $("body").toggleClass("sidebar-collapse sidebar-mini");
-  });
+	// Click Sidebar Toggle
+	$(".sidebar-toggle").click(function() {
+		/* Act on the event */
+		$("body").toggleClass("sidebar-collapse sidebar-mini");
+	});
 
+	/** Block SideBar Category **/
+	$('.block-sidebar .list-category li.has-child a').click(function () {
+		$(this).next().slideToggle();
+		$(this).parent().toggleClass('open');
+		return false;
+	});
+
+	$(document).ready(function(){
+		$(".sidebar-menu").addClass("tree");
+		// $(".active.treeview").addClass("menu-open");
+		$(".treeview a").click(function(){
+			$(this).next().slideToggle();
+			$(this).parent().toggleClass("menu-open");
+		});
+	});
 
 })(jQuery); // End of use strict
