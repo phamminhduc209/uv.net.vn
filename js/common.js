eab6d0ca94f6074d056f50a958d908a63edf9ec9
Duc Pham <minhducpham.it@gmail.com>
2017-06-13 06:45:01 +0700

    add

--- js/common.js
+++ js/common.js
@@ -4,26 +4,22 @@
 	// Select picker
 	$('.selectpicker').selectpicker({});
 
-	// Click Sidebar Toggle
 	$(".sidebar-toggle").click(function() {
-		/* Act on the event */
 		$("body").toggleClass("sidebar-collapse sidebar-mini");
 	});
 
-	/** Block SideBar Category **/
-	$('.block-sidebar .list-category li.has-child a').click(function () {
-		$(this).next().slideToggle();
-		$(this).parent().toggleClass('open');
-		return false;
-	});
+	// $('.block-sidebar .list-category li.has-child a').click(function () {
+	// 	$(this).next().slideToggle();
+	// 	$(this).parent().toggleClass('open');
+	// 	return false;
+	// });
 
-	$(document).ready(function(){
-		$(".sidebar-menu").addClass("tree");
-		// $(".active.treeview").addClass("menu-open");
-		$(".treeview a").click(function(){
-			$(this).next().slideToggle();
-			$(this).parent().toggleClass("menu-open");
-		});
-	});
+	// 	$(".sidebar-menu").addClass("tree");
+	// 	// $(".active.treeview").addClass("menu-open");
+	// 	$(".treeview a").click(function(){
+	// 		$(this).next().slideToggle();
+	// 		$(this).parent().toggleClass("menu-open");
+	// 	});
+	// });
 
 })(jQuery); // End of use strict
